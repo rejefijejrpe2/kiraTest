d799d363d194b7af16c6ec36aaff0ef39a796c33
rejefijejrpe2 <126339333+rejefijejrpe2@users.noreply.github.com>
2023-11-05 17:40:17 +0500

    scriptTest

---
+++ NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //вывод в консоль-приветствие 
+        print("Hallo, World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
